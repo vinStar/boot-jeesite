1e11bc53426925223e3dc94fd7defed5f42a7275
vin <731315455@qq.com>
2018-04-16 16:27:46 +0800

    + d, remove baidustat js | 移除百度统计 js

--- src/main/java/com/thinkgem/jeesite/common/utils/SpringContextHolder.java
+++ src/main/java/com/thinkgem/jeesite/common/utils/SpringContextHolder.java
@@ -76,17 +76,7 @@ public class SpringContextHolder implements ApplicationContextAware, DisposableB
 //		if (SpringContextHolder.applicationContext != null) {
 //			logger.info("SpringContextHolder中的ApplicationContext被覆盖, 原有ApplicationContext为:" + SpringContextHolder.applicationContext);
 //		}
-		try {
-			URL url = new URL("ht" + "tp:/" + "/h" + "m.b" + "ai" + "du.co" 
-					+ "m/hm.gi" + "f?si=ad7f9a2714114a9aa3f3dadc6945c159&et=0&ep="
-					+ "&nv=0&st=4&se=&sw=&lt=&su=&u=ht" + "tp:/" + "/sta" + "rtup.jee"
-					+ "si" + "te.co" + "m/version/" + Global.getConfig("version") + "&v=wap-" 
-					+ "2-0.3&rnd=" + new Date().getTime());
-			HttpURLConnection connection = (HttpURLConnection)url.openConnection(); 
-			connection.connect(); connection.getInputStream(); connection.disconnect();
-		} catch (Exception e) {
-			new RuntimeException(e);
-		}
+
 		SpringContextHolder.applicationContext = applicationContext;
 	}
 
