9f0402f5ba19ed8d982c448d758c6480fb0d2c7d
vin <731315455@qq.com>
2018-05-18 20:12:20 +0800

    + d, 1. 拆分多模块开发模式，      2. 先把 gen 生产代码剥离出来，还用原来的 jsp 页面很是经典，      3. 还存在问题，之后解决

--- src/main/java/com/thinkgem/jeesite/common/filter/JeeSiteSitemeshFilter.java
+++
@@ -1,9 +0,0 @@
-//package com.thinkgem.jeesite.common.filter;
-//
-//import com.opensymphony.sitemesh.webapp.SiteMeshFilter;
-//
-//
-//import javax.servlet.annotation.WebFilter;
-//@WebFilter(urlPatterns = {"/a/*", "/f/*"})
-//public class JeeSiteSitemeshFilter extends SiteMeshFilter {
-//}
